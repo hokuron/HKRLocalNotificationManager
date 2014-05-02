ad6fac119f74840abe861cd8805a74c7d6ede3e2
shimizu <anti.soft.b@gmail.com>
2014-05-02 15:33:05 +0900

    Add: scheduleNotificationOn:alertBody:userInfo: method and its tests

--- HRKLocalNotificationManager/HKRLocalNotificationManager.m
+++ HRKLocalNotificationManager/HKRLocalNotificationManager.m
@@ -1,6 +1,5 @@
 //
 //  HKRLocalNotificationManager.m
-//  Example
 //
 //  Created by Takuma Shimizu on 5/1/14.
 //  Copyright (c) 2014 Takuma Shimizu. All rights reserved.
@@ -9,6 +8,12 @@
 #import "HKRLocalNotificationManager.h"
 
 #import <objc/runtime.h>
+
+@interface HKRLocalNotificationManager ()
+
+@property (nonatomic, strong) UIApplication *app;
+
+@end
 
 @implementation HKRLocalNotificationManager
 
@@ -28,6 +33,7 @@
 {
     if (! (self = [super init])) return nil;
     _fixedSoundName = UILocalNotificationDefaultSoundName;
+    _app = [UIApplication sharedApplication];
     return self;
 }
 
@@ -39,7 +45,27 @@
 
 #pragma mark - Publics
 
+- (UILocalNotification *)scheduleNotificationOn:(NSDate *)fireDate body:(NSString *)alertBody userInfo:(NSDictionary *)userInfo options:(NSDictionary *)otherProperties
+{
+    NSDictionary *options = [self.class mergeNotificationProperty:otherProperties fireDate:fireDate alertBody:alertBody userInfo:userInfo];
+    UILocalNotification *notif = [UILocalNotification hkr_localNotificationWithOptions:options];
+    [self.app scheduleLocalNotification:notif];
+    return notif;
+}
+
 #pragma mark - Privates
+
++ (NSDictionary *)mergeNotificationProperty:(NSDictionary *)properties fireDate:(NSDate *)fireDate alertBody:(NSString *)alertBody userInfo:(NSDictionary *)userInfo
+{
+    NSMutableDictionary *options = properties ? [properties mutableCopy] : [@{} mutableCopy];
+    if (! [[options allKeys] containsObject:@"soundName"]) {
+        [options setObject:[HKRLocalNotificationManager sharedManager].fixedSoundName forKey:@"soundName"];
+    }
+    [options setObject:fireDate forKey:@"fireDate"];
+    [options setObject:alertBody forKey:@"alertBody"];
+    [options setObject:userInfo forKey:@"userInfo"];
+    return [options copy];
+}
 
 @end
 
--- Tests/HKRLocalNotificationManagerTests.m
+++ Tests/HKRLocalNotificationManagerTests.m
@@ -25,48 +25,70 @@
 @property (nonatomic)       NSInteger      applicationIconBadgeNumber;
 @property (nonatomic, copy) NSDictionary   *userInfo;
 
-@property (nonatomic) NSMutableDictionary *props;
-
 @end
 
-@implementation HKRLocalNotificationManagerTests
+@implementation HKRLocalNotificationManagerTests {
+    NSMutableDictionary *_props;
+    HKRLocalNotificationManager *_manager;
+}
 
 - (void)setUp
 {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
-    _fireDate = [NSDate date];
-    _timeZone = [NSTimeZone localTimeZone];
-    _repeatInterval = NSCalendarUnitDay | NSCalendarUnitHour;
-    _repeatCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSISO8601Calendar];
-    _alertBody = @"body";
-    _hasAction = NO;
-    _alertAction = @"action";
-    _alertLaunchImage = @"path/to/image";
-    _soundName = @"custom_sound";
+    _fireDate                   = [[NSDate date] dateByAddingTimeInterval:24.0 * 60.0 * 60.0];
+    _timeZone                   = [NSTimeZone localTimeZone];
+    _repeatInterval             = NSCalendarUnitDay | NSCalendarUnitHour;
+    _repeatCalendar             = [[NSCalendar alloc] initWithCalendarIdentifier:NSISO8601Calendar];
+    _alertBody                  = @"body";
+    _hasAction                  = NO;
+    _alertAction                = @"action";
+    _alertLaunchImage           = @"path/to/image";
+    _soundName                  = @"custom_sound";
     _applicationIconBadgeNumber = 86;
-    _userInfo = @{@"user": @"info"};
-    
+    _userInfo                   = @{@"user": @"info"};
+
     [self buildProperties];
+    
+    _manager = [HKRLocalNotificationManager sharedManager];
 }
 
 - (void)tearDown
 {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [_props removeAllObjects];
+    [[UIApplication sharedApplication] cancelAllLocalNotifications];
     [super tearDown];
 }
 
-- (void)buildProperties
+- (NSArray *)propertyNames
 {
-    _props = [@{} mutableCopy];
+    NSMutableArray *names = [@[] mutableCopy];
     unsigned int count;
     objc_property_t *properties = class_copyPropertyList([self class], &count);
     for (unsigned int idx=0; idx < count; idx += 1) {
         objc_property_t property = properties[idx];
         NSString *name = [NSString stringWithUTF8String:property_getName(property)];
-        if ([name isEqualToString:@"props"]) continue;
-        [self.props setObject:[self valueForKey:name] forKey:name];
+        [names addObject:name];
+    }
+    return [names copy];
+}
+
+- (void)buildProperties
+{
+    _props = [@{} mutableCopy];
+    NSArray *propNames = [self propertyNames];
+    for (NSString *key in propNames) {
+        [_props setObject:[self valueForKey:key] forKey:key];
+    }
+}
+
+- (void)localNotificationPropertiesMatchingTestWithTarget:(UILocalNotification *)target testData:(UILocalNotification *)notif
+{
+    NSArray *propNames = [self propertyNames];
+    for (NSString *name in propNames) {
+        if ([name isEqualToString:@"soundName"]) continue;
+        XCTAssertEqualObjects([target valueForKey:name], [notif valueForKey:name], @"%@ should match %@", name, [self valueForKey:name]);
     }
 }
 
@@ -81,31 +103,75 @@
     XCTAssertThrows([HKRLocalNotificationManager new], @"new method should not recognize");
 }
 
+- (void)testScheduleNotificationOnBodyUserInfoOptions_noOptions
+{
+    UILocalNotification *result = [_manager scheduleNotificationOn:self.fireDate body:self.alertBody userInfo:self.userInfo options:nil];
+    UILocalNotification *notif  = [UILocalNotification new];
+    notif.fireDate  = self.fireDate;
+    notif.alertBody = self.alertBody;
+    notif.userInfo  = self.userInfo;
+    
+    [self localNotificationPropertiesMatchingTestWithTarget:result testData:notif];
+    [self localNotificationPropertiesMatchingTestWithTarget:result testData:[[UIApplication sharedApplication].scheduledLocalNotifications firstObject]];
+}
+
+- (void)testScheduleNotificationOnBodyUserInfoOptions_addOptions
+{
+    NSString *alertLaunchImage = @"additional_option";
+    UILocalNotification *result = [_manager scheduleNotificationOn:self.fireDate body:self.alertBody userInfo:self.userInfo options:@{@"alertLaunchImage": alertLaunchImage}];
+    UILocalNotification *notif  = [UILocalNotification new];
+    notif.fireDate  = self.fireDate;
+    notif.alertBody = self.alertBody;
+    notif.userInfo  = self.userInfo;
+    notif.alertLaunchImage = alertLaunchImage;
+    
+    [self localNotificationPropertiesMatchingTestWithTarget:result testData:notif];
+    [self localNotificationPropertiesMatchingTestWithTarget:result testData:[[UIApplication sharedApplication].scheduledLocalNotifications firstObject]];
+}
+
+- (void)testScheduleNotificationOnBodyUserInfoOptions_overwriteArgProps
+{
+    NSString *alertBody = @"orverwritten_alert_body";
+    UILocalNotification *result = [_manager scheduleNotificationOn:self.fireDate body:self.alertBody userInfo:self.userInfo options:@{@"alertBody": alertBody}];
+    UILocalNotification *notif  = [UILocalNotification new];
+    notif.fireDate  = self.fireDate;
+    notif.alertBody = self.alertBody;
+    notif.userInfo  = self.userInfo;
+    
+    [self localNotificationPropertiesMatchingTestWithTarget:result testData:notif];
+    [self localNotificationPropertiesMatchingTestWithTarget:result testData:[[UIApplication sharedApplication].scheduledLocalNotifications firstObject]];
+    XCTAssertNotEqualObjects(self.alertBody, alertBody, @"cannot overwrite argument properties by options");
+}
+
+- (void)testScheduleNotificationOnBodyUserInfoOptions_fixedSoundName
+{
+    _manager.fixedSoundName = @"fixedSoundName";
+    UILocalNotification *result = [_manager scheduleNotificationOn:self.fireDate body:self.alertBody userInfo:self.userInfo options:nil];
+    XCTAssertEqualObjects(result.soundName, _manager.fixedSoundName, @"if soundName is NOT specified, it should match %@", _manager.fixedSoundName);
+}
+
+- (void)testScheduleNotificationOnBodyUserInfoOptions_overwriteFixedSoundName
+{
+    NSString *soundName = @"orverwritten_sound_name";
+    UILocalNotification *result = [_manager scheduleNotificationOn:self.fireDate body:self.alertBody userInfo:self.userInfo options:@{@"soundName": soundName}];
+    XCTAssertEqualObjects(result.soundName, soundName, @"if soundName is specified, fixedSoundName is ignored (specific soundName: %@)", soundName);
+}
+
 - (void)testUILocalNotificationCategory_hkr_localNotificationWithOptions_validArg
 {
-    UILocalNotification *notif = [UILocalNotification hkr_localNotificationWithOptions:self.props];
-    
-    unsigned int count;
-    objc_property_t *properties = class_copyPropertyList([self class], &count);
-    for (unsigned int idx=0; idx < count; idx += 1) {
-        objc_property_t property = properties[idx];
-        NSString *name = [NSString stringWithUTF8String:property_getName(property)];
-        if ([name isEqualToString:@"props"]) continue;
+    UILocalNotification *notif = [UILocalNotification hkr_localNotificationWithOptions:_props];
+    NSArray *propNames = [self propertyNames];
+    for (NSString *name in propNames) {
         XCTAssertEqualObjects([notif valueForKey:name], [self valueForKey:name], @"%@ should match %@", name, [self valueForKey:name]);
     }
 }
 
 - (void)testUILocalNotificationCategory_hkr_localNotificationWithOptions_invalidArg
 {
-    [self.props setObject:@"no_exists_property" forKey:@"invalid"];
-    UILocalNotification *notif = [UILocalNotification hkr_localNotificationWithOptions:self.props];
-    
-    unsigned int count;
-    objc_property_t *properties = class_copyPropertyList([self class], &count);
-    for (unsigned int idx=0; idx < count; idx += 1) {
-        objc_property_t property = properties[idx];
-        NSString *name = [NSString stringWithUTF8String:property_getName(property)];
-        if ([name isEqualToString:@"props"]) continue;
+    [_props setObject:@"no_exists_property" forKey:@"invalid"];
+    UILocalNotification *notif = [UILocalNotification hkr_localNotificationWithOptions:_props];
+    NSArray *propNames = [self propertyNames];
+    for (NSString *name in propNames) {
         XCTAssertEqualObjects([notif valueForKey:name], [self valueForKey:name], @"%@ should match %@", name, [self valueForKey:name]);
     }
 }
